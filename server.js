d7dead79a3ec582dbcfc490900a72154d7b6483d
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-03-19 18:10:22 +0530

    mod 7,, sending response for first time

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const http = require("http")
 const server = http.createServer((req,res)=>{
-    console.log("Ayasha")
+    console.log( "Welcome to my Node Js project")
+    if(req.url==="/home"){
+        res.write("welcome home")
+    }
+    else if(req.url==="/about"){
+        res.write("Welcome to About Us page")
+    }
+    else if(req.url==="/node"){
+        res.write("Welcome to my Node Js project")
+    }
+    res.end()
 })
 server.listen(4000)
