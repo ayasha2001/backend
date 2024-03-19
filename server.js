93ab4a1e8009c9d929650d455bbd75a1610c0f3d
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-03-19 18:57:08 +0530

    message sent to backend

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const http = require("http")
-const server = http.createServer((req,res)=>{
-    console.log( "Welcome to my Node Js project")
-    if(req.url==="/home"){
-        res.write("welcome home")
-    }
-    else if(req.url==="/about"){
-        res.write("Welcome to About Us page")
-    }
-    else if(req.url==="/node"){
-        res.write("Welcome to my Node Js project")
-    }
-    res.end()
-})
-server.listen(4000)
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">send</button></form>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (req.url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedbody = Buffer.concat(body).toString();
+      const message = parsedbody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+});
+
+server.listen(4000);
